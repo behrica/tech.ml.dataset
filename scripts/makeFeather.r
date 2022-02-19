8702df4d381f50487796a805197fb3f78184406a
Carsten Behring <carsten.behring@gmail.com>
2022-02-19 10:33:46 +0100

    fixed data type

--- scripts/makeFeather.r
+++ scripts/makeFeather.r
@@ -8,9 +8,9 @@ data$x1 <- as.factor(data$x1)
 data$x2 <- as.character(data$x2)
 data$x3 <- as.integer(data$x3)
 data$x4 <- as.numeric(c(8.0,9.0,10.0,11.0))
-data$x5 <- as.POSIXlt("1/1/2020")
+data$x5 <- as.POSIXct("1/1/2020")
 
 arrow::write_feather(data,"/tmp/data.feather_arrow")
 feather::write_feather(data,"/tmp/data.feather_feather")
 arrow::write_ipc_stream(data,"/tmp/data.ipc_stream")
-arrow::write_arrow(data,"/tmp/data.arrow") ; same as arrow::write_feather
+arrow::write_arrow(data,"/tmp/data.arrow") # same as arrow::write_feather
