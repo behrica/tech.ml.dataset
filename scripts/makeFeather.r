2b2b4d2aa5a963a68ee0e2a82a905680b3f2bf8e
Carsten Behring <carsten.behring@gmail.com>
2022-02-19 10:30:31 +0100

    added rscript

---
+++ scripts/makeFeather.r
@@ -0,0 +1,16 @@
+#install.packages(c("arrow","feather"),repos = "http://cran.us.r-project.org")
+
+
+data <- data.frame(x1 = c(1, 5, 8, 2),
+                   x2 = c(3, 2, 5, 2),
+                   x3 = c(2, 7, 1, 2))
+data$x1 <- as.factor(data$x1)
+data$x2 <- as.character(data$x2)
+data$x3 <- as.integer(data$x3)
+data$x4 <- as.numeric(c(8.0,9.0,10.0,11.0))
+data$x5 <- as.POSIXlt("1/1/2020")
+
+arrow::write_feather(data,"/tmp/data.feather_arrow")
+feather::write_feather(data,"/tmp/data.feather_feather")
+arrow::write_ipc_stream(data,"/tmp/data.ipc_stream")
+arrow::write_arrow(data,"/tmp/data.arrow") ; same as arrow::write_feather
